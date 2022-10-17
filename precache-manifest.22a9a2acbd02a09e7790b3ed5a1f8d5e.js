6fc64213d9c3ed9269fb3be3ea1f4c065937f951
Mritunjay Kumar <83032964+Mritunjay1711@users.noreply.github.com>
2022-10-17 20:59:33 +0530

    Updates

---
+++ precache-manifest.22a9a2acbd02a09e7790b3ed5a1f8d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cb26f07dd18e2bf0158",
+    "url": "/TextUtils-React/static/css/main.d5948fcb.chunk.css"
+  },
+  {
+    "revision": "0cb26f07dd18e2bf0158",
+    "url": "/TextUtils-React/static/js/main.ba3e672f.chunk.js"
+  },
+  {
+    "revision": "118d5bb5acde121ee0e8",
+    "url": "/TextUtils-React/static/js/runtime~main.6ef670dd.js"
+  },
+  {
+    "revision": "9162cf41c7fa6eafdcbb",
+    "url": "/TextUtils-React/static/js/2.518c7694.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/TextUtils-React/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "e3e659114a336fda126277a89d6dde11",
+    "url": "/TextUtils-React/index.html"
+  }
+];
